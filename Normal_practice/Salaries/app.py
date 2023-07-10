42ec27b954a132c013cb5140d535b2afe400794a
h4ck4l1 <105937725+h4ck4l1@users.noreply.github.com>
2023-07-10 13:06:41 +0530

    Update app.py

--- Normal_practice/Salaries/app.py
+++ Normal_practice/Salaries/app.py
@@ -3,14 +3,14 @@ from dash import Dash,dcc,html,page_container,page_registry
 from glob import glob
 import subprocess
 
-files = glob("./Salaries/pages/*.ipynb")
+files = glob("/content/Salaries/pages/*.ipynb")
 
 for f in files:
     py_file = "." + f.split('.')[-2] + ".py"
     subprocess.run("colab-convert "+f+" "+py_file,shell=True)
 
 
-app = Dash(__name__,use_pages=True,pages_folder='Salaries/pages')
+app = Dash(__name__,use_pages=True,pages_folder='/content/Salaries/pages')
 
 app.layout = html.Div([
     html.H1("بسمله حرهما نراهم",style={'color':'green'}),
