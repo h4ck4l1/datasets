c25a8db0cbbd4b090178418235052d935821c8d8
h4ck4l1 <105937725+h4ck4l1@users.noreply.github.com>
2023-07-10 14:35:09 +0530

    Add files via upload

---
+++ Normal_practice/Salaries/pages/Statistical_analysis.py
@@ -0,0 +1,351 @@
+# %%
+# !! {"metadata":# !! {}
+import numpy as np
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+pio.templates['mod'] = go.layout.Template(layout=dict(font=dict(family="Fira Code")))
+pio.templates.default = "plotly_dark+mod"
+from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
+from scipy import stats
+import scipy.stats as ss
+from dash import Dash,html,dcc,Input,Output,callback,register_page,dash_table
+#<cc-ac> %matplotlib inline
+
+# %%
+# !! {"metadata":# !! {}
+salaries = pd.read_csv('https://github.com/h4ck4l1/datasets/raw/main/Normal_practice/ds_salaries.csv')
+
+# %%
+# !! {"metadata":# !! {}
+salaries['work_year'] = salaries.work_year.astype(str)
+
+# %%
+# !! {"metadata":# !! {}
+salaries.head()
+
+# %%
+# !! {"metadata":# !! {}
+salaries.drop(['job_title','salary_currency','salary'],inplace=True,axis=1)
+
+# %%
+# !! {"metadata":# !! {}
+salaries.head()
+
+# %%
+# !! {"metadata":# !! {}
+salaries.columns
+
+# %%
+# !! {"metadata":# !! {}
+idx = pd.IndexSlice
+
+# %%
+# !! {"metadata":# !! {}
+years = salaries.work_year.unique()
+exp_levels = salaries.experience_level.unique()
+emp_types = salaries.employment_type.unique()
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Message
+"""
+
+# %%
+# !! {"metadata":# !! {}
+msgs = '''
+The statistical analysis has been carried out here and explained in brief terms  
+The tests used are  
+- [Levene's](https://en.wikipedia.org/wiki/Levene%27s_test) test to test for equalitly of variances  
+- [Oneway Anova](https://en.wikipedia.org/wiki/One-way_analysis_of_variance) to test for equality of means in k-groups but one variable  
+
+'''
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Mean Analysis
+"""
+
+# %%
+# !! {"metadata":# !! {}
+Mean_Analysis = make_subplots(rows=2,cols=2,horizontal_spacing=0.2,vertical_spacing=0.2)
+for i,year in enumerate(salaries.work_year.unique()):
+    sal = salaries.query(f"work_year == '{year}'").salary_in_usd.to_numpy()
+    sal = StandardScaler().fit_transform(sal.reshape(-1,1)).ravel()
+    qq = sm.qqplot(sal,line='s').gca().lines
+    plt.close()
+    Mean_Analysis.add_trace(go.Scatter(x=qq[0].get_xdata(),y=qq[0].get_ydata(),mode="markers"),row=((i+1)//3)+1,col=(i%2)+1)
+    Mean_Analysis.add_trace(go.Scatter(x=qq[1].get_xdata(),y=qq[1].get_ydata(),mode="lines"),row=((i+1)//3)+1,col=(i%2)+1)
+    Mean_Analysis.add_annotation(text=f"{year}",font=dict(size=20),bordercolor="white",showarrow=False,x=0,xanchor="left",xref='x domain',y=1.1,yref='y domain',yanchor="bottom",row=((i+1)//3)+1,col=(i%2)+1)
+    Mean_Analysis.add_annotation(text=f"count: {len(sal)}",font=dict(size=20),bordercolor="white",showarrow=False,x=0.8,xanchor="center",y=1.1,yref='y domain',yanchor="bottom",row=((i+1)//3)+1,col=(i%2)+1)
+
+Mean_Analysis.update_layout(width=1000,height=700,showlegend=False)
+Mean_Analysis.show()
+
+# %%
+# !! {"metadata":# !! {}
+year_2020 = salaries.query("work_year == '2020'").salary_in_usd.to_numpy()
+year_2021 = salaries.query("work_year == '2021'").salary_in_usd.to_numpy()
+year_2022 = salaries.query("work_year == '2022'").salary_in_usd.to_numpy()
+year_2023 = salaries.query("work_year == '2023'").salary_in_usd.to_numpy()
+
+# %%
+# !! {"metadata":# !! {}
+mean_inf = '''
+The Higher count Years are very much approximated to nomral plot but the 76 tends to skew.
+As the lesser count plot is concave up w.r.t to the normal q-q line means that it is right skewed.
+but as it can be assumed to that 2020 is also derived from normal population so we can apply manova test to see whether there is any statistical significance between the means
+Doing variance test
+
+- **levene's test**
+- - we get pvalue=0.20411, we conclude that variances are equal
+- **anova test**
+- - we get pvalue = 9.9e-48, we conclude that there is significant difference in means
+
+
+'''
+
+# %%
+# !! {"metadata":# !! {}
+from scipy.stats import levene
+
+# %%
+# !! {"metadata":# !! {}
+levene(year_2020,year_2021,year_2022,year_2023)
+
+# %%
+# !! {"metadata":# !! {}
+sm.stats.anova_oneway((year_2020,year_2021,year_2022,year_2023),use_var='equal')
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Experience Level Mean Analysis
+"""
+
+# %%
+# !! {"metadata":# !! {}
+sal = salaries.copy()
+sal.set_index(['work_year','experience_level'],inplace=True)
+sal.sort_index(inplace=True)
+exp_level_mean = make_subplots(rows=4,cols=4)
+sal.loc[sal.index.unique()[0]]
+for i,year_exp in enumerate(sal.index.unique()):
+    vals = StandardScaler().fit_transform(sal.loc[year_exp].salary_in_usd.to_numpy().reshape(-1,1)).ravel()
+    data = sm.qqplot(vals,line='s').gca().lines
+    plt.close()
+    exp_level_mean.add_trace(go.Scatter(x=data[0].get_xdata(),y=data[0].get_ydata(),mode="markers"),row=(i//4)+1,col=(i%4)+1)
+    exp_level_mean.add_trace(go.Scatter(x=data[1].get_xdata(),y=data[1].get_ydata(),mode="lines"),row=(i//4)+1,col=(i%4)+1)
+    exp_level_mean.add_annotation(text=f"{year_exp}",font=dict(size=15),x=0.8,y=1.05,xanchor="center",yanchor="bottom",xref='x domain',yref='y domain',bordercolor="white",showarrow=False,row=(i//4)+1,col=(i%4)+1)
+    exp_level_mean.add_annotation(text=f"Count :{len(data[0].get_xdata())}",font=dict(size=15),x=0.3,y=1.05,xanchor="center",yanchor="bottom",xref='x domain',yref='y domain',bordercolor="white",row=(i//4)+1,col=(i%4)+1,showarrow=False)
+
+exp_level_mean.update_layout(height=800,hovermode=False,showlegend=False)
+
+# %%
+# !! {"metadata":# !! {}
+tab_vals = salaries.groupby(['work_year','experience_level']).aggregate({'salary_in_usd':'mean'}).unstack(level=1)
+tab_vals.reset_index(inplace=True)
+
+# %%
+# !! {"metadata":# !! {}
+l = []
+for item in tab_vals.to_dict('records'):
+    d = {}
+    for key,value in item.items():
+        d[key[1]] = value
+    l.append(d)
+
+# %%
+# !! {"metadata":# !! {}
+table1 = dash_table.DataTable(
+        data=l,
+        columns=[{'name': [col[0],col[1]], 'id': col[1]} for col in tab_vals.columns],
+        merge_duplicate_headers=True,
+        style_header={'textAlign':'center'},
+        style_cell={'textAlign':'left'}
+    )
+
+# %%
+# !! {"metadata":# !! {}
+exp_level_mean_inf = '''
+Assumptions:  
+  - Independent samples are collected (i.e., every instance of one category is independent of another instance of that category regardless of other categories)  
+
+As we can see we can only carry out parametric statistical anlysis for equal means only in case of year 2022 and 2023 between their respective experience levels.
+This is due to the fact the representative sample size for earlier years is very low and highly skewed and requires non-paramtric analysis which is less reliable than parametric.
+Hence carrying out Parametrice analysis
+we use levene's test to determine the hypothesis of equal variances and then do the one way anova for hypothesis of equal means
+we get 
+
+For 2020
+- Non-Parametric Kruskal-wallis test p = 0.0016
+
+For 2021
+- Non-Parametric Kruskal-wallis test p = 2.8e-12
+
+
+For 2022
+- levenes test
+  - p value = 0.13823, indicating equality of variances
+- anova one way 
+  -  p value = 2.6223e-77, indicating inequality of means
+
+For 2023
+- levenes test
+  - p value = 0.0471, indicating equality of variances
+- anova one way 
+  -  p value = 1.24e-42, indicating inequality of means  
+
+**Therefore we conclude that All the Means across their experience levels are statistically different**
+'''
+
+# %%
+# !! {"metadata":# !! {}
+for y in ['2022','2023']:
+    levene(
+        sal.loc[idx[y,exp_levels[0]],'salary_in_usd'].to_numpy(),
+        sal.loc[idx[y,exp_levels[1]],'salary_in_usd'].to_numpy(),
+        sal.loc[idx[y,exp_levels[2]],'salary_in_usd'].to_numpy(),
+        sal.loc[idx[y,exp_levels[3]],'salary_in_usd'].to_numpy()
+        )[1]
+
+# %%
+# !! {"metadata":# !! {}
+sm.stats.anova_oneway((
+    sal.loc[idx['2022',exp_levels[0]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2022',exp_levels[1]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2022',exp_levels[2]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2022',exp_levels[3]],'salary_in_usd'].to_numpy()
+    ),use_var="equal")[1]
+
+# %%
+# !! {"metadata":# !! {}
+sm.stats.anova_oneway((
+    sal.loc[idx['2023',exp_levels[0]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2023',exp_levels[1]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2023',exp_levels[2]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2023',exp_levels[3]],'salary_in_usd'].to_numpy()
+    ),use_var="unequal")[1]
+
+# %%
+# !! {"metadata":# !! {}
+stats.kruskal(
+    sal.loc[idx['2020',exp_levels[0]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2020',exp_levels[1]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2020',exp_levels[2]],'salary_in_usd'].to_numpy(),
+    sal.loc[idx['2020',exp_levels[3]],'salary_in_usd'].to_numpy()
+)
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Employment Type
+"""
+
+# %%
+# !! {"metadata":# !! {}
+salaries.head()
+
+# %%
+# !! {"metadata":# !! {}
+sal = salaries.copy()
+sal.set_index(['work_year','employment_type'],inplace=True)
+sal.sort_index(inplace=True)
+
+# %%
+# !! {"metadata":# !! {}
+emp_type_mean = make_subplots(rows=4,cols=4)
+for i,year_emp in enumerate(sal.index.unique()):
+    stan = StandardScaler().fit_transform(sal.loc[year_emp,'salary_in_usd'].to_numpy().reshape(-1,1)).ravel()
+    data = sm.qqplot(stan,line='s').gca().lines
+    plt.close()
+    emp_type_mean.add_trace(go.Scatter(x=data[0].get_xdata(),y=data[0].get_ydata(),mode="markers"),row=(i//4)+1,col=(i%4)+1)
+    emp_type_mean.add_trace(go.Scatter(x=data[1].get_xdata(),y=data[1].get_ydata(),mode="lines"),row=(i//4)+1,col=(i%4)+1)
+    emp_type_mean.add_annotation(text=f"{year_emp}",font=dict(size=15),x=0.3,y=1.05,xref="x domain",yref='y domain',xanchor='left',yanchor='bottom',row=(i//4)+1,col=(i%4)+1,bordercolor="white",showarrow=False)
+    emp_type_mean.add_annotation(text=f"count :{len(data[0].get_xdata())}",font=dict(size=15),x=0.8,y=1.05,xref="x domain",yref='y domain',xanchor='left',yanchor='bottom',row=(i//4)+1,col=(i%4)+1,bordercolor="white",showarrow=False)
+
+emp_type_mean.update_layout(height=800,showlegend=False,hovermode=False)
+emp_type_mean.show()
+
+
+# %%
+# !! {"metadata":# !! {}
+exp_type_mean_inf = '''
+As we can observe the disparity between the respective same level values is high due to the lower count, which make the analysis non parametric   
+we can still do welschs anova which will be less reliable than the normal anova
+so we rest the case that year level inferences between employment types is avoided
+'''
+
+# %%
+# !! {"metadata":# !! {}
+
+register_page(__name__,order=3,path='/pages/Statistical_analysis',name="Statistial Analysis",title="Statistical Analysis")
+
+
+options = ['Message','Mean Salary Analysis','Experience Level Mean Analysis']
+
+
+layout = html.Div([
+    html.H1("Statistical Analysis",style={'font-face':'Fira Code'}),
+    dcc.Dropdown(
+        id='drop1',
+        options=options,
+        value='Message',    
+        searchable=True,
+        placeholder="Select options...",
+        style={'font-family':'Fira Code'}
+    ),
+    dcc.Loading(id="Loading1",children=[
+        html.Div(id="graphing"),
+        dcc.Markdown(id='msg1',style={'font-family':'Fira Code'})
+        ],type="cube")
+])
+
+
+@callback(
+    [Output('graphing','children'),Output('msg1','children')],
+    [Input('drop1','value')]
+)
+def update_graph(drop1):
+    if drop1 == options[0]:
+        time.sleep(2)
+        return None,msgs
+    elif drop1 == options[1]:
+        time.sleep(2)
+        return dcc.Graph(figure=Mean_Analysis),mean_inf
+    elif drop1 == options[2]:
+        time.sleep(2)
+        return dcc.Graph(figure=exp_level_mean),exp_level_mean_inf
+
+# %%
+# !! {"metadata":# !! {}
+
+
+# %%
+# !! {"main_metadata":{
+# !!   "kernelspec": {
+# !!     "display_name": "base",
+# !!     "language": "python",
+# !!     "name": "python3"
+# !!   },
+# !!   "language_info": {
+# !!     "codemirror_mode": {
+# !!       "name": "ipython",
+# !!       "version": 3
+# !!     },
+# !!     "file_extension": ".py",
+# !!     "mimetype": "text/x-python",
+# !!     "name": "python",
+# !!     "nbconvert_exporter": "python",
+# !!     "pygments_lexer": "ipython3",
+# !!     "version": "3.9.13"
+# !!   },
+# !!   "orig_nbformat": 4
+# !! }}
---
+++ Normal_practice/Salaries/pages/sal.py
@@ -0,0 +1,391 @@
+# %%
+# !! {"metadata":# !! {}
+import time
+import pycountry as pyc
+from IPython.display import clear_output
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
+from plotly.subplots import make_subplots
+pio.templates['mod'] = go.layout.Template(layout=dict(font=dict(family="Fira Code")))
+pio.templates.default = "plotly_dark+mod"
+from dash import html,dcc,callback,Input,Output,dash_table,register_page
+
+# %%
+# !! {"metadata":# !! {}
+sal = pd.read_csv('https://github.com/h4ck4l1/datasets/raw/main/Normal_practice/ds_salaries.csv')
+salaries = sal.copy()
+
+# %%
+# !! {"metadata":# !! {}
+sal.head()
+
+# %%
+# !! {"metadata":# !! {}
+alpha2_names = sal.company_location.unique()
+all_counts = {country.alpha_2:country.name for country in pyc.countries}
+alpha_dict = {}
+for country in alpha2_names:
+    alpha_dict[country] = pyc.countries.get(alpha_2=country).name
+
+alpha2_names_1 = sal.employee_residence.unique()
+all_counts = {country.alpha_2:country.name for country in pyc.countries}
+alpha_dict_1 = {}
+for country in alpha2_names_1:
+    alpha_dict_1[country] = pyc.countries.get(alpha_2=country).name
+
+# %%
+# !! {"metadata":# !! {}
+sal.drop(['job_title','salary','salary_currency'],axis=1,inplace=True)
+sal['work_year'] = sal.work_year.astype(str)
+sal['experience_level'] = sal.experience_level.map({'SE':'Senior','MI':'MidInter','EX':'Executive','EN':'Entry'})
+sal['employment_type'] = sal.employment_type.map({'FT':'FullTime','CT':'Contract','FL':'FreeLancer','PT':'PartTime'})
+sal['company_location'] = sal.company_location.map(alpha_dict)
+sal['employee_residence'] = sal.employee_residence.map(alpha_dict_1)
+sal_by_exp = sal.groupby(['work_year','experience_level']).aggregate({'salary_in_usd':'mean'}).salary_in_usd
+sal_by_emp = sal.groupby(['work_year','employment_type']).aggregate({'salary_in_usd':'mean'}).salary_in_usd
+exp_list = ['Entry','MidInter','Senior','Executive']
+emp_list = ['FullTime','Contract','FreeLancer','PartTime']
+years = ['2020','2021','2022','2023']
+
+# %%
+# !! {"metadata":# !! {}
+salaries['experience_level'] = salaries.experience_level.map({'SE':'Senior','MI':'MidInter','EX':'Executive','EN':'Entry'})
+salaries['employment_type'] = salaries.employment_type.map({'FT':'FullTime','CT':'Contract','FL':'FreeLancer','PT':'PartTime'})
+salaries['company_location'] = salaries.company_location.map(alpha_dict)
+salaries['employee_residence'] = salaries.employee_residence.map(alpha_dict_1)
+
+# %%
+# !! {"metadata":# !! {}
+idx = pd.IndexSlice
+
+# %%
+# !! {"metadata":# !! {}
+sal_by_exp_mean = sal.groupby('work_year').aggregate({'salary_in_usd':'mean'})
+sal_by_exp_mean['shifted_salary'] = sal_by_exp_mean.shift(1).fillna(0).loc[:,'salary_in_usd']
+sal_by_exp_mean['net_minus'] = sal_by_exp_mean['salary_in_usd'] - sal_by_exp_mean['shifted_salary']
+sal_by_exp_mean.loc['2020','shifted_salary'] = 1
+sal_by_exp_mean['change'] = (sal_by_exp_mean.net_minus/sal_by_exp_mean.shifted_salary)*100
+sal_by_exp_mean.loc['2020','change'] = 0
+
+# %%
+# !! {"metadata":# !! {}
+new_col_names = ['Work Year','Experience Level','Employment Type','Job Title','Salary','Salary Currency','Salary in USD','Employee Residence','Remote ratio','Company Location','Company Size']
+new_col_dict = {}
+for i in range(len(new_col_names)):
+    new_col_dict[salaries.columns[i]] = new_col_names[i]
+
+# %%
+# !! {"metadata":# !! {}
+col_names = ['Work Year','Experience Level','Employment Type','Salary in USD','Employee Residence','Remote ratio','Company Location','Company Size']
+
+# %%
+# !! {"metadata":# !! {}
+col_dict = {}
+for i in range(len(col_names)):
+    col_dict[sal.columns[i]] = col_names[i]
+
+# %%
+# !! {"metadata":# !! {}
+len(sal)
+
+# %%
+# !! {"metadata":# !! {}
+"""
+# Dashtable
+"""
+
+# %%
+# !! {"metadata":# !! {}
+tab = dash_table.DataTable(
+id='Table',
+data=salaries.to_dict('records'),
+columns=[{'name':new_col_dict[i],'id':i,'deletable':True,'selectable':True} for i in salaries.columns],
+page_size=10,
+filter_action='native',
+sort_action='native',
+row_selectable="multi")
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Experience YoY
+"""
+
+# %%
+# !! {"metadata":# !! {}
+exp_yoy = go.Figure()
+for exp in exp_list:
+    exp_yoy.add_trace(go.Bar(x=years,y=sal_by_exp.loc[idx[:,exp]],name=f"{exp}",text=f"{exp}",textfont=dict(size=20),texttemplate="%{text}<br>%{y:.2s}"))
+exp_yoy.update_layout(barmode='stack',height=600)
+exp_yoy.update_xaxes(tickfont=dict(size=18),title=dict(text="Work Year",font=dict(size=25)))
+exp_yoy.update_yaxes(tickfont=dict(size=18),title=dict(text="Mean in USD",font=dict(size=25)))
+exp_yoy.add_annotation(text="Experiece Level Salary in USD YoY",showarrow=False,font=dict(size=30),xanchor="center",yanchor="bottom",xref='x domain',yref='y domain',y=1.05,bordercolor="white",borderpad=10)
+clear_output()
+
+# %%
+# !! {"metadata":# !! {}
+"""
+#### Experience Inference
+"""
+
+# %%
+# !! {"metadata":# !! {}
+Experience_inference = "Graph goes up and up good!"
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Employment YoY
+"""
+
+# %%
+# !! {"metadata":# !! {}
+emp_yoy = go.Figure()
+for emp in emp_list:
+    emp_yoy.add_trace(go.Bar(x=years,y=sal_by_emp.loc[idx[:,emp]],name=f"{emp}",text=f"{emp}",textfont=dict(size=20),texttemplate="%{text}<br>%{y:.2s}"))
+emp_yoy.update_layout(barmode='stack',height=650)
+emp_yoy.update_xaxes(tickfont=dict(size=18),title=dict(text="Work Year",font=dict(size=25)))
+emp_yoy.update_yaxes(tickfont=dict(size=18),title=dict(text="Mean by Type in USD",font=dict(size=25)))
+emp_yoy.add_annotation(text="Employment Type Salary in USD YoY",showarrow=False,font=dict(size=30),xanchor="center",yanchor="bottom",xref='x domain',yref='y domain',y=1.05,bordercolor="white")
+clear_output()
+
+# %%
+# !! {"metadata":# !! {}
+"""
+#### Employment Inference
+"""
+
+# %%
+# !! {"metadata":# !! {}
+Employment_inference = "Employment Type mean shots up at 2021 for contract"
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Reduced Mean YoY
+"""
+
+# %%
+# !! {"metadata":# !! {}
+mean_yoy = go.Figure()
+mean_yoy.add_annotation(text="Reduced Mean with %Change wrt to previous year",showarrow=False,font=dict(size=30),xanchor="center",yanchor="bottom",xref='x domain',yref='y domain',y=1.05,bordercolor="white",borderpad=10)
+mean_yoy.add_trace(go.Scatter(x=years,y=sal.groupby(['work_year']).aggregate({'salary_in_usd':'mean'}),mode='lines+markers+text',line=dict(dash='dashdot',color="springgreen"),marker=dict(size=50,opacity=0.3),name='Mean YoY',textposition="top center",textfont=dict(size=15)))
+mean_yoy.add_trace(go.Bar(x=years,y=sal.groupby(['work_year']).aggregate({'salary_in_usd':'mean'}),marker=dict(color="royalblue"),name="Mean YoY",text=sal_by_exp_mean.change,texttemplate="<br>\n<br>\n<br>\n<b><i>Mean :%{y:.2s}<br>%Change:%{text:.3s}</i></b>",textposition="inside",textfont=dict(size=20,color="rgb(225,145,65)")))
+mean_yoy.update_traces(width=0.7,selector=dict(type="bar"))
+mean_yoy.update_layout(height=700,width=1200)
+mean_yoy.update_xaxes(showline=False,griddash="dash",gridcolor="rgba(255,255,255,0.2)",tickfont=dict(size=20),title="Work Year",titlefont=dict(size=20))
+mean_yoy.update_yaxes(range=[0,2e5],griddash="dash",gridcolor="rgba(255,255,255,0.2)",tickfont=dict(size=20),title="Mean reduced YoY",titlefont=dict(size=20))
+clear_output()
+
+# %%
+# !! {"metadata":# !! {}
+"""
+#### Mean Inference
+"""
+
+# %%
+# !! {"metadata":# !! {}
+Mean_inference = "There was a good jump from 2021 to 2022 which can mean the employment along with payscale gradually increased, but it can also be due to growing interest towards datascience field ingeneral"
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Remote Ratio
+"""
+
+# %%
+# !! {"metadata":# !! {}
+sal_by_rem = pd.pivot_table(sal,index=['work_year'],columns=['remote_ratio'],values=['salary_in_usd'],aggfunc=['count','mean'])
+
+# %%
+# !! {"metadata":# !! {}
+sal_by_rem
+
+# %%
+# !! {"metadata":# !! {}
+remote_pie = go.Figure()
+remote_pie.add_trace(go.Pie(values=sal_by_rem.loc['2020',idx['count','salary_in_usd']],labels=[0,50,100],texttemplate="<b>Remote Ratio</b>:<b>%{label}</b><br>count:%{value}<br>%{text:.2s}</br>",textfont=dict(size=20),sort=False,direction="clockwise",textposition="outside",text=sal_by_rem.loc['2020',idx['mean','salary_in_usd']]))
+frames = []
+steps = []
+for year in years:
+    frames.append(go.Frame(data=[go.Pie(values=sal_by_rem.loc[year,idx['count','salary_in_usd']],labels=[0,50,100],texttemplate="<b>Remote Ratio</b>:<b>%{label}</b><br>count:%{value}<br>%{text:.2s}</br>",textfont=dict(size=20),sort=False,direction="clockwise",textposition="outside",text=sal_by_rem.loc[year,idx['mean','salary_in_usd']])],layout=dict(autosize=False),name=year))
+    steps.append(dict(
+        args=[[year],dict(frame=dict(duration=1500,redraw=True),mode="immediate",transition=dict(duration=1500,easing="quadratic-in-out"))],
+        label=year,
+        method="animate"
+    ))
+remote_pie.update(frames=frames)
+remote_pie.update_layout(
+    updatemenus=[dict(
+        buttons=[dict(label="Play",method="animate",args=[None,dict(frame=dict(duration=1500,redraw=True),fromcurrent=True,transition=dict(duration=1500,easing="circle-in-out"))])],
+        xanchor="left",
+        yanchor="top",
+        x=0.1,
+        y=0,
+        showactive=False,
+        type="buttons"
+    )],
+    sliders=[dict(
+        steps=steps,
+        xanchor="left",
+        yanchor="top",
+        x=0,
+        y=-0.09,
+        len=0.9,
+        active=0,
+        currentvalue=dict(
+            font=dict(size=20),
+            prefix="Year :",
+            xanchor="right",
+            visible=True
+        ),
+        transition=dict(duration=1500,easing="cubic-in-out"),
+        pad=dict(r=10,b=10),
+        font=dict(size=20)
+    )]
+)
+remote_pie.update_layout(autosize=False,height=600,width=1100,margin=dict(t=5,r=10),legend=dict(font=dict(size=20),xanchor="right",yanchor="top",x=0,y=0.05))
+clear_output()
+
+# %%
+# !! {"metadata":# !! {}
+"""
+#### Remote ratio inference
+"""
+
+# %%
+# !! {"metadata":# !! {}
+Remote_ratio = "As it can be seen that people who worked remotely were higher in 2020 and eventually due to Covid reasons and companies started to reopen and employees gradually started working in office again"
+
+# %%
+# !! {"metadata":# !! {}
+"""
+## Company Location
+"""
+
+# %%
+# !! {"metadata":# !! {}
+sal_by_comp = sal.groupby(['company_location']).aggregate({'salary_in_usd':'mean'})
+q_25 = sal_by_comp.salary_in_usd.quantile(0.25)
+q_75 = sal_by_comp.salary_in_usd.quantile(0.75)
+
+# %%
+# !! {"metadata":# !! {}
+q_25_countries = sal_by_comp.loc[sal_by_comp['salary_in_usd'] < q_25].index
+q_75_countries = sal_by_comp.loc[sal_by_comp['salary_in_usd'] > q_75].index
+sal['q'] = np.NaN
+sal.loc[sal['company_location'].isin(q_25_countries),'q'] = 1
+sal.loc[sal['company_location'].isin(q_75_countries),'q'] = 3
+sal.fillna(2,inplace=True)
+
+# %%
+# !! {"metadata":# !! {}
+company_loc = make_subplots(rows=3,cols=1,row_heights=[0.25,0.5,0.25],vertical_spacing=0.03,row_titles=['Companies below First Quantile','Companies in between First and Third Quantile','Companies in Third Quatile'])
+company_loc.add_trace(go.Histogram(x=sal.loc[sal.q==1,'salary_in_usd'],y=sal.loc[sal.q==1,'company_location'],name="First Quantile",orientation='h',histfunc='avg'),row=1,col=1)
+company_loc.add_trace(go.Histogram(x=sal.loc[sal.q==2,'salary_in_usd'],y=sal.loc[sal.q==2,'company_location'],name="Between First and Third Quantile",orientation='h',histfunc='avg'),row=2,col=1)
+company_loc.add_trace(go.Histogram(x=sal.loc[sal.q==3,'salary_in_usd'],y=sal.loc[sal.q==3,'company_location'],name="Third Quantile",orientation='h',histfunc='avg'),row=3,col=1)
+company_loc.update_yaxes(categoryorder="total descending",title="Country where company is located",title_font_size=20)
+company_loc.update_xaxes(title="Mean salary in USD",title_font_size=20)
+company_loc.update_annotations(font=dict(size=30))
+company_loc.update_layout(height=2500)
+clear_output()
+
+# %%
+# !! {"metadata":# !! {}
+"""
+#### Company Location Inference
+"""
+
+# %%
+# !! {"metadata":# !! {}
+pd.crosstab(index=sal.company_location,columns=sal.work_year).loc["United States"]
+
+# %%
+# !! {"metadata":# !! {}
+sal.groupby('work_year').count()
+
+# %%
+# !! {"metadata":# !! {}
+Company_inf = '''
+The Mean salary of Israel is highest followed by Puero Rico and United States, though the count of United states is far higher than rest, so most of the mean salaries can be highly skewed
+'''
+
+# %%
+# !! {"metadata":# !! {}
+
+register_page(__name__,order=2,path='/pages/sal',name="Visual Presentation",title="Visual Presentation")
+
+layout = html.Div([
+    html.Br(),
+    dcc.Dropdown(
+        id='drop',
+        options=['Table','Experience YoY','Employment YoY','Mean reduced YoY','Remote Ratio YoY','Company Location'],
+        value='Table',
+        style={'backgroundColor':'black','color':'white','font-family':'Fira Code'}
+    ),
+    dcc.Loading(id="loading",children=[
+        html.Div(id='Table_or_graphs'),
+        html.Br(),
+        html.Br(),
+        html.H2(id='Inf',style={'font-family':'Fira Code'}),
+        dcc.Markdown(id='msg',style={'white-space':'pre','font-family':'Fira Code'})
+    ],type='cube')
+])
+
+@callback(
+    [
+        Output('Table_or_graphs','children'),
+        Output('Inf','children'),
+        Output('msg','children')
+        ],
+    [Input('drop','value')]
+)
+def update_graph(value):
+
+    if value == 'Table':
+        time.sleep(2)
+        return tab,None,None
+    elif value == 'Experience YoY':
+        time.sleep(2)
+        return dcc.Graph(figure=exp_yoy),"Inferences :",Experience_inference
+    elif value == 'Employment YoY':
+        time.sleep(2)
+        return dcc.Graph(figure=emp_yoy),"Inferences :",Employment_inference
+    elif value == 'Mean reduced YoY':
+        time.sleep(2)
+        return dcc.Graph(figure=mean_yoy),"Inferences :",Mean_inference
+    elif value == 'Remote Ratio YoY':
+        time.sleep(2)
+        return dcc.Graph(figure=remote_pie),"Inferences :",Remote_ratio
+    elif value == 'Company Location':
+        time.sleep(2)
+        return dcc.Graph(figure=company_loc),"Inferences :",Company_inf
+    
+
+# %%
+# !! {"main_metadata":{
+# !!   "kernelspec": {
+# !!     "display_name": "base",
+# !!     "language": "python",
+# !!     "name": "python3"
+# !!   },
+# !!   "language_info": {
+# !!     "codemirror_mode": {
+# !!       "name": "ipython",
+# !!       "version": 3
+# !!     },
+# !!     "file_extension": ".py",
+# !!     "mimetype": "text/x-python",
+# !!     "name": "python",
+# !!     "nbconvert_exporter": "python",
+# !!     "pygments_lexer": "ipython3",
+# !!     "version": "3.9.13"
+# !!   },
+# !!   "orig_nbformat": 4
+# !! }}
