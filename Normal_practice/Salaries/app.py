efaff760f01d9a12afacdde159ee67a26c9ec49d
h4ck4l1 <105937725+h4ck4l1@users.noreply.github.com>
2023-07-10 14:37:21 +0530

    Add files via upload

--- Normal_practice/Salaries/app.py
+++ Normal_practice/Salaries/app.py
@@ -1,16 +1,7 @@
-#!/home/sohail/anaconda3/bin/python
 from dash import Dash,dcc,html,page_container,page_registry
-from glob import glob
-import subprocess
-
-files = glob("/content/Salaries/pages/*.ipynb")
-
-for f in files:
-    py_file = f.split('.')[-2] + ".py"
-    subprocess.run("colab-convert "+f+" "+py_file,shell=True)
 
 
-app = Dash(__name__,use_pages=True,pages_folder='/content/Salaries/pages')
+app = Dash(__name__,use_pages=True,pages_folder='./pages')
 
 app.layout = html.Div([
     html.H1("بسمله حرهما نراهم",style={'color':'green'}),
