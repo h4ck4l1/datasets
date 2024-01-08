a00bd9b250154125552088228e78c07a52d66c9b
h4ck4l1 <105937725+h4ck4l1@users.noreply.github.com>
2024-01-08 05:12:09 +0000

    asta

--- convert_csv_tot_tfrec.py
+++ convert_csv_tot_tfrec.py
@@ -89,7 +89,60 @@ def BPPMatrix(val):
     return Feature(bytes_list=BytesList(value=[val]))
 
 def Length(val):
-    val = serialize_tensor(tf.constant(val,tf.float32)).numpy()
+    val = serialize_tensor(tf.constant(len(val),tf.float32)).numpy()
     return Feature(bytes_list=BytesList(value=[val]))
 
+def serialize_row(row_2a3,row_dms):
 
+    assert (row_2a3[seq_id] == row_dms[seq_id]) and (row_2a3[seq] == row_dms[seq])
+
+    feature = dict(
+        seq_id = SequenceId(row_2a3[seq_id]),
+        seq = Sequence(row_2a3[seq]),
+        reads_2a3 = Reads(row_2a3[reads]),
+        reads_dms = Reads(row_dms[reads]),
+        sn_filter_2a3 = SNFilter(row_2a3[sn_filter]),
+        sn_filter_dms = SNFilter(row_dms[sn_filter]),
+        dataset_name_2a3 = DatasetName(row_2a3[dataset_name]),
+        dataset_name_dms = DatasetName(row_dms[dataset_name]),
+        signal_to_noise_2a3 = SignalToNoise(row_2a3[signal_to_noise]),
+        signal_to_noise_dms = SignalToNoise(row_dms[signal_to_noise]),
+        reactivity_2a3 = Reactivity(row_2a3[reactivity]),
+        reactivity_dms = Reactivity(row_dms[reactivity]),
+        reactivity_error_2a3 = ReactivityError(row_2a3[reactivity_error]),
+        reactivity_error_dms = ReactivityError(row_dms[reactivity_error]),
+        length = Length(row_2a3[seq]),
+        bpp_matrix = BPPMatrix(row_2a3[seq]),
+        bracket_seq = BracketSequence(row_2a3[seq])
+    )
+
+    return Example(features=Features(feature=feature))
+
+def sharded_tfrecords(exitstack,base_path,num_shards):
+    output_tfrecord_filenames = [f"{base_path}/train_{idx}" for idx in range(num_shards)]
+    return [exitstack.enter_context(TFRecordWriter(file_name,options="GZIP")) for file_name in output_tfrecord_filenames]
+
+l = len(df_2a3)
+num_shards = 150
+output_filebase = "content/tf_records"
+
+def process_row(index,row_2a3,row_dms,output_tfrecords):
+    tf_example = serialize_row(row_2a3,row_dms)
+    shard_index = num_shards % index
+    output_tfrecords[shard_index].write(tf_example.SerializeToString()) 
+
+
+def paralleliz_tfrecord_creation(df_2a3,df_dms,output_tfrecords,num_shards):
+    with ProcessPoolExecutor() as executor:
+        futures = []
+        for index, (row_2a3,row_dms) in enumerate(tqdm(zip(df_2a3.itertuples(index=False),df_dms.itertuples(index=False)))):
+            futures.append(executor.submit(process_row,index,row_2a3,row_dms,output_tfrecords))
+
+        wait(futures)
+
+
+
+if __name__ == "__main__":
+    with contextlib2.ExitStack() as exit_stack:
+        output_tfrecords = sharded_tfrecords(exit_stack,output_filebase,num_shards)
+        paralleliz_tfrecord_creation(df_2a3,df_dms,output_tfrecords,num_shards)
